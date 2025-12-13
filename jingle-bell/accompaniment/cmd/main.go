f29a8677c1cfe540f0498568303726025086f3f7
masatomo57 <akagawa.code@gmail.com>
2025-12-13 22:28:04 +0900

    accompanimentの書き込みの整理

--- jingle-bell/accompaniment/cmd/main.go
+++ jingle-bell/accompaniment/cmd/main.go
@@ -15,59 +15,61 @@ const (
 	end = 1.0e-1
 )
 
+type Accompaniment []struct {
+	conf.Chord
+	Length float64
+}
+
 func main() {
 	file := "out.bin"
 	f, _ := os.Create(file)
 	defer f.Close()
 
-	// Jingle bells, jingle bells
-	generate(conf.ChordC, 4, f) // C
-	generate(conf.ChordC, 4, f) // C
+	accompaniment := Accompaniment{
+		// Jingle bells, jingle bells
+		{Chord: conf.ChordC, Length: 4},
+		{Chord: conf.ChordC, Length: 4},
+		// Jingle all the way
+		{Chord: conf.ChordC, Length: 4},
+		{Chord: conf.ChordC, Length: 4},
+		// Oh what fun it is to ride
+		{Chord: conf.ChordF, Length: 4},
+		{Chord: conf.ChordC, Length: 4},
+		// In a one-horse open sleigh
+		{Chord: conf.ChordD7, Length: 4},
+		{Chord: conf.ChordG, Length: 4},
+		// Jingle bells, jingle bells
+		{Chord: conf.ChordC, Length: 4},
+		{Chord: conf.ChordC, Length: 4},
+		// Jingle all the way
+		{Chord: conf.ChordC, Length: 4},
+		{Chord: conf.ChordC, Length: 4},
+		// Oh what fun it is to ride
+		{Chord: conf.ChordF, Length: 4},
+		{Chord: conf.ChordC, Length: 4},
+		// In a one-horse open sleigh
+		{Chord: conf.ChordG7, Length: 4},
+		{Chord: conf.ChordC, Length: 4},
+	}
 
-	// Jingle all the way
-	generate(conf.ChordC, 4, f) // C
-	generate(conf.ChordC, 4, f) // C
-
-	// Oh what fun it is to ride
-	generate(conf.ChordF, 4, f) // F
-	generate(conf.ChordC, 4, f) // C
-
-	// In a one-horse open sleigh
-	generate(conf.ChordD7, 4, f) // D7
-	generate(conf.ChordG, 4, f)  // G
-
-	// Jingle bells, jingle bells
-	generate(conf.ChordC, 4, f) // C
-	generate(conf.ChordC, 4, f) // C
-
-	// Jingle all the way
-	generate(conf.ChordC, 4, f) // C
-	generate(conf.ChordC, 4, f) // C
-
-	// Oh what fun it is to ride
-	generate(conf.ChordF, 4, f) // F
-	generate(conf.ChordC, 4, f) // C
-
-	// In a one-horse open sleigh
-	generate(conf.ChordG7, 4, f) // G7
-	generate(conf.ChordC, 4, f)  // C
+	accompaniment.WriteTo(f)
 }
 
-func generate(chord conf.Chord, soundLength float64, file *os.File) {
-	samples := int((soundLength * samplesPerSec) / 4)
-	damping := math.Pow(end, 1.0/float64(samples))
-	for i := 0; i < samples; i++ {
-		notesNum := len(chord.Notes)
-		vol := 1.0 / float64(notesNum)
-		var sample float64
-		for j := 0; j < notesNum; j++ {
-			sample += vol * math.Sin((tau*chord.Notes[j].Hertz()*float64(i))/samplesPerSec)
+func (a Accompaniment) WriteTo(file *os.File) {
+	for _, chordWithLength := range a {
+		samples := int((chordWithLength.Length * samplesPerSec) / 4)
+		damping := math.Pow(end, 1.0/float64(samples))
+		for i := 0; i < samples; i++ {
+			notesNum := len(chordWithLength.Chord.Notes)
+			vol := 1.0 / float64(notesNum)
+			var sample float64
+			for j := 0; j < notesNum; j++ {
+				sample += vol * math.Sin((tau*chordWithLength.Chord.Notes[j].Hertz()*float64(i))/samplesPerSec)
+			}
+			sample = sample * math.Pow(damping, float64(i))
+			buf := make([]byte, 4)
+			binary.LittleEndian.PutUint32(buf, math.Float32bits(float32(sample)))
+			file.Write(buf)
 		}
-		sample = sample * math.Pow(damping, float64(i))
-		buf := make([]byte, 4)
-
-		// バイト順序=LittleEndian
-		binary.LittleEndian.PutUint32(buf, math.Float32bits(float32(sample)))
-		file.Write(buf)
 	}
 }
