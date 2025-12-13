ee7c6ac02c29526924173396644b42bf21bc9a17
masatomo57 <akagawa.code@gmail.com>
2025-12-13 22:23:48 +0900

    melody書き込み周りの整理

--- jingle-bell/melody/cmd/main.go
+++ jingle-bell/melody/cmd/main.go
@@ -15,91 +15,89 @@ const (
 	end = 1.0e-1
 )
 
+type Melody []struct {
+	conf.Note
+	Length float64
+}
+
 func main() {
 	file := "out.bin"
 	f, _ := os.Create(file)
 	defer f.Close()
 
-	// Jingle bells, jingle bells
-	generateMelody(conf.E5, 1, f)
-	generateMelody(conf.E5, 1, f)
-	generateMelody(conf.E5, 2, f)
+	melody := Melody{
+		// Jingle bells, jingle bells
+		{Note: conf.E5, Length: 1},
+		{Note: conf.E5, Length: 1},
+		{Note: conf.E5, Length: 2},
+		{Note: conf.E5, Length: 1},
+		{Note: conf.E5, Length: 1},
+		{Note: conf.E5, Length: 2},
+		// Jingle all the way
+		{Note: conf.E5, Length: 1},
+		{Note: conf.G5, Length: 1},
+		{Note: conf.C5, Length: 1.5},
+		{Note: conf.D5, Length: 0.5},
+		{Note: conf.E5, Length: 4},
+		// Oh what fun it is to ride
+		{Note: conf.F5, Length: 1},
+		{Note: conf.F5, Length: 1},
+		{Note: conf.F5, Length: 1.5},
+		{Note: conf.F5, Length: 0.5},
+		{Note: conf.F5, Length: 1},
+		{Note: conf.E5, Length: 1},
+		{Note: conf.E5, Length: 1},
+		{Note: conf.E5, Length: 1},
+		// In a one-horse open sleigh
+		{Note: conf.E5, Length: 1},
+		{Note: conf.D5, Length: 1},
+		{Note: conf.D5, Length: 1},
+		{Note: conf.E5, Length: 1},
+		{Note: conf.D5, Length: 2},
+		{Note: conf.G5, Length: 2},
+		// Jingle bells, jingle bells
+		{Note: conf.E5, Length: 1},
+		{Note: conf.E5, Length: 1},
+		{Note: conf.E5, Length: 2},
+		{Note: conf.E5, Length: 1},
+		{Note: conf.E5, Length: 1},
+		{Note: conf.E5, Length: 2},
+		// Jingle all the way
+		{Note: conf.E5, Length: 1},
+		{Note: conf.G5, Length: 1},
+		{Note: conf.C5, Length: 1.5},
+		{Note: conf.D5, Length: 0.5},
+		{Note: conf.E5, Length: 4},
+		// Oh what fun it is to ride
+		{Note: conf.F5, Length: 1},
+		{Note: conf.F5, Length: 1},
+		{Note: conf.F5, Length: 1.5},
+		{Note: conf.F5, Length: 0.5},
+		{Note: conf.F5, Length: 1},
+		{Note: conf.E5, Length: 1},
+		{Note: conf.E5, Length: 1},
+		{Note: conf.E5, Length: 1},
+		// In a one-horse open sleigh
+		{Note: conf.G5, Length: 1},
+		{Note: conf.G5, Length: 1},
+		{Note: conf.F5, Length: 1},
+		{Note: conf.D5, Length: 1},
+		{Note: conf.C5, Length: 4},
+	}
 
-	generateMelody(conf.E5, 1, f)
-	generateMelody(conf.E5, 1, f)
-	generateMelody(conf.E5, 2, f)
-
-	// Jingle all the way
-	generateMelody(conf.E5, 1, f)
-	generateMelody(conf.G5, 1, f)
-	generateMelody(conf.C5, 1.5, f)
-	generateMelody(conf.D5, 0.5, f)
-	generateMelody(conf.E5, 4, f)
-
-	// Oh what fun it is to ride
-	generateMelody(conf.F5, 1, f)
-	generateMelody(conf.F5, 1, f)
-	generateMelody(conf.F5, 1.5, f)
-	generateMelody(conf.F5, 0.5, f)
-
-	generateMelody(conf.F5, 1, f)
-	generateMelody(conf.E5, 1, f)
-	generateMelody(conf.E5, 1, f)
-	generateMelody(conf.E5, 1, f)
-
-	// In a one-horse open sleigh
-	generateMelody(conf.E5, 1, f)
-	generateMelody(conf.D5, 1, f)
-	generateMelody(conf.D5, 1, f)
-	generateMelody(conf.E5, 1, f)
-	generateMelody(conf.D5, 2, f)
-	generateMelody(conf.G5, 2, f)
-
-	// Jingle bells, jingle bells
-	generateMelody(conf.E5, 1, f)
-	generateMelody(conf.E5, 1, f)
-	generateMelody(conf.E5, 2, f)
-
-	generateMelody(conf.E5, 1, f)
-	generateMelody(conf.E5, 1, f)
-	generateMelody(conf.E5, 2, f)
-
-	// Jingle all the way
-	generateMelody(conf.E5, 1, f)
-	generateMelody(conf.G5, 1, f)
-	generateMelody(conf.C5, 1.5, f)
-	generateMelody(conf.D5, 0.5, f)
-	generateMelody(conf.E5, 4, f)
-
-	// Oh what fun it is to ride
-	generateMelody(conf.F5, 1, f)
-	generateMelody(conf.F5, 1, f)
-	generateMelody(conf.F5, 1.5, f)
-	generateMelody(conf.F5, 0.5, f)
-
-	generateMelody(conf.F5, 1, f)
-	generateMelody(conf.E5, 1, f)
-	generateMelody(conf.E5, 1, f)
-	generateMelody(conf.E5, 1, f)
-
-	// In a one-horse open sleigh
-	generateMelody(conf.G5, 1, f)
-	generateMelody(conf.G5, 1, f)
-	generateMelody(conf.F5, 1, f)
-	generateMelody(conf.D5, 1, f)
-	generateMelody(conf.C5, 4, f)
+	melody.WriteTo(f)
 }
 
-func generateMelody(note conf.Note, soundLength float64, file *os.File) {
-	samples := int((soundLength * samplesPerSec) / 4)
-	damping := math.Pow(end, 1.0/float64(samples))
-	for i := 0; i < samples; i++ {
-		sample := math.Sin((tau * note.Hertz() * float64(i)) / float64(samplesPerSec))
-		sample = sample * math.Pow(damping, float64(i))
-		buf := make([]byte, 4)
-
-		// バイト順序=LittleEndian
-		binary.LittleEndian.PutUint32(buf, math.Float32bits(float32(sample)))
-		file.Write(buf)
+func (m Melody) WriteTo(file *os.File) {
+	for _, noteWithLength := range m {
+		samples := int((noteWithLength.Length * samplesPerSec) / 4)
+		damping := math.Pow(end, 1.0/float64(samples))
+		for i := 0; i < samples; i++ {
+			sample := math.Sin((tau * noteWithLength.Note.Hertz() * float64(i)) / float64(samplesPerSec))
+			sample = sample * math.Pow(damping, float64(i))
+			buf := make([]byte, 4)
+			binary.LittleEndian.PutUint32(buf, math.Float32bits(float32(sample)))
+			file.Write(buf)
+		}
 	}
 }
